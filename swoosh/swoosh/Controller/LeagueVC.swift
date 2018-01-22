72c5e98551d4da7c7868a9913f7dfdd4090fd4e7
George Buckley <g.buckley@live.com>
2018-01-22 21:00:39 +0000

    Programatic Segue

--- swoosh/swoosh/Controller/LeagueVC.swift
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +22,8 @@ class LeagueVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onNextTap(_ sender: Any) {
+        performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
-    */
 
 }
