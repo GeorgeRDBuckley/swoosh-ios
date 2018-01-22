81143ec087d9138db4141dc1d8c58b7e09b10256
George Buckley <g.buckley@live.com>
2018-01-22 21:51:29 +0000

    Created player struct

--- swoosh/swoosh/Controller/LeagueVC.swift
+++ swoosh/swoosh/Controller/LeagueVC.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextButton: borderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +27,23 @@ class LeagueVC: UIViewController {
     @IBAction func onNextTap(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
-
+    
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "men")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
+    
 }
---
+++ swoosh/swoosh/Model/player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  swoosh
+//
+//  Created by George Buckley on 22/01/2018.
+//  Copyright © 2018 George Buckley. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var skillLevel: String?
+}
